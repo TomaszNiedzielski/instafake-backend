eee1dc4041658c190dca1850e8215803416f502b
Tomasz <tomasz9531@gmail.com>
2020-12-29 13:44:06 +0100

    Bind classes

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register Interface and Repository in here
+        // You must place Interface in first place
+        // If you dont, the Repository will not get readed.
+        $this->app->bind(
+            'App\Interfaces\ImageInterface',
+            'App\Repositories\ImageRepository'
+        );
+
+        $this->app->bind(
+            'App\Interfaces\UserInterface',
+            'App\Repositories\UserRepository'
+        );
+
+        $this->app->bind(
+            'App\Interfaces\FollowInterface',
+            'App\Repositories\FollowRepository'
+        );
+    }
+}
