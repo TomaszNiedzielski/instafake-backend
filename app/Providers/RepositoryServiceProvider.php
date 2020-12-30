82b30baeba14bd37b328ffb5074607d1e1e0512c
Tomasz <tomasz9531@gmail.com>
2020-12-30 21:35:07 +0100

    Fixed bind classes

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -25,5 +25,10 @@ class RepositoryServiceProvider extends ServiceProvider
             'App\Interfaces\FollowInterface',
             'App\Repositories\FollowRepository'
         );
+
+        $this->app->bind(
+            'App\Interfaces\AvatarInterface',
+            'App\Repositories\AvatarRepository'
+        );
     }
 }
